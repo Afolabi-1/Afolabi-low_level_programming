54b5e8a7d12b748bc31456ace4478ba641f755b9
Afolabi Adetunji <adetunjiafolabi05@gmail.com>
2022-06-30 20:14:57 -0700

    created script four

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+* puts: print out a string of words
+*Return: Always 0 (success)
+*/
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
