cfa740a9c4638f1d819c0d5fffc6c02ff5d7512a
Afolabi Adetunji <adetunjiafolabi05@gmail.com>
2022-06-30 20:06:09 -0700

    fixed error

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 
+/* This should return a 0*/
+
 int main(void)
 {
-	printf ("with proper grammar, but the outcome is a piece of art,\n");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
