d979297db858c20b88618dd7d20a3bdc127528cc
Afolabi Adetunji <adetunjiafolabi05@gmail.com>
2022-06-30 21:16:31 -0700

    fixed typo in script 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -14,7 +14,7 @@ int main(void)
 	long int li;
 	long long int lli;
 
-	printf("size of a char: %zu byte(s))\n", sizeof(c));
+	printf("size of a char: %zu byte(s)\n", sizeof(c));
 	printf("size of an int: %zu byte(s)\n", sizeof(i));
 	printf("size of a long int: %zu byte(s)\n", sizeof(li));
 	printf("size of a long long int: %zu byte(s)\n", sizeof(lli));
