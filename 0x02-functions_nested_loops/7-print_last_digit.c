90b2a7c41cdaecc77c9d48e58569e0b138ed1392
Afolabi Adetunji <adetunjiafolabi05@gmail.com>
2022-07-05 21:40:58 -0700

    script 7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ *  print_last_digit - prints the last digit of a number
+ *  @num: last digti
+ *  Return: always 0
+ */
+
+int print_last_digit(int num)
+{
+	return num % 10;
+}
