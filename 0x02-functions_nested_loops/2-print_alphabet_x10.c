3c1937ad26b8032bcb768a033ae6b54ca2c79dc4
Afolabi Adetunji <adetunjiafolabi05@gmail.com>
2022-07-05 20:32:59 -0700

    script 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - prints the alphabets ten times
+ *
+ * Return: always 0
+ */
+
+void print_alphabet_x10(void)
+{
+	int i = 0, j;
+
+	/* Defines how many times */
+	while (i <= 10)
+	{
+		for (j = 'a'; j <= 'z'; j++)
+		{
+			_putchar(j);
+		}
+		_putchar('\n');
+		i++;
+	}
+}
