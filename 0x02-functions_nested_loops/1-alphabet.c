dfef28c89fdc4af9f8b505c4760113de8abc2d1f
Afolabi Adetunji <adetunjiafolabi05@gmail.com>
2022-07-05 20:17:55 -0700

    created script 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints alphabet in lowercase
+ *
+ * Return: always 0
+ */
+
+void print_alphabet(void)
+{
+	char var1;
+
+	for (var1 = 'a'; var1 < 'z'; var1++)
+	{
+		_putchar(var1);
+	}
+	_putchar('\n');
+}
