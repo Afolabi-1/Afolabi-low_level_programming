f18202932c66400c658a1555c5327bc1195025d8
Afolabi Adetunji <adetunjiafolabi05@gmail.com>
2022-07-07 03:54:17 -0700

    script 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - multiplies two integers
+ * @a: character to be multiplied
+ * @b: character
+ * Return: result of a* b
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
