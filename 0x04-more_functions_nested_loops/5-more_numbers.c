41497dc107a26ba5fe2a01b879abe59bf8429b64
Afolabi Adetunji <adetunjiafolabi05@gmail.com>
2022-07-07 10:38:22 -0700

    script 5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints 10 times the num for 0 - 14
+ *
+ * Return: always 0
+ */
+
+void more_numbers(void)
+{
+	int c = 0, b;
+
+	/* first loop */
+	while (c < 11)
+	{
+		/* second loop */
+		for (b = 0; b < 10; b++)
+		{
+			_putchar(b / 10);
+			_putchar((b % 10) + '0');
+		}
+		_putchar('\n');
+		c++;
+	}
+}
