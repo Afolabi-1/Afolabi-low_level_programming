fb1761c8596a2506957fbdacafc21d5f65f399c0
Afolabi Adetunji <adetunjiafolabi05@gmail.com>
2022-07-07 20:41:10 -0700

    fixed error

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -5,7 +5,7 @@
  * @n: char
  */
 
-void print_line(int n)
+void print_diagonal(int n)
 {
 	int i;
 
@@ -16,6 +16,6 @@ void print_line(int n)
 	}
 
 	for (i = 0; i < n; i++)
-		_putchar('_');
+		_putchar('\\');
 	_putchar('\n');
 }
