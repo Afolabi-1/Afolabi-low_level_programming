3de78c9c9518bd0d384c9ff975a0a7c421829992
Afolabi Adetunji <adetunjiafolabi05@gmail.com>
2022-07-07 20:35:05 -0700

    script 7

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -3,20 +3,19 @@
 /**
  * print_diagonal - draws a diagonal line in the terminal
  * @n: char
- * Return: 0
  */
 
-void print_diagonal(int n)
+void print_line(int n)
 {
 	int i;
 
-	if (n < 0)
+	if (n <= 0)
 	{
 		_putchar('\n');
 		return;
 	}
 
 	for (i = 0; i < n; i++)
-		_putchar('\\');
+		_putchar('_');
 	_putchar('\n');
 }
