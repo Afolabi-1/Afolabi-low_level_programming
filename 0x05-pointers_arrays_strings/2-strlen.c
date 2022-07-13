9deb10d29cf507f8cee5383dcd4a111ecda9762a
Afolabi Adetunji <adetunjiafolabi05@gmail.com>
2022-07-13 14:42:36 -0700

    script 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _strlen - return the length of a string
+ * @s: variable
+ * Return: always 0
+ */
+
+int _strlen(char *s)
+{
+	int i;
+
+	for(i = 0; s[i];)
+		i++;
+
+	return(i);
+}
