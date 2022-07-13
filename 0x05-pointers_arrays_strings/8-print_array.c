89fc2cfd4cee35aa05cbb55e4b99dead8d336caa
Afolabi Adetunji <adetunjiafolabi05@gmail.com>
2022-07-13 16:44:49 -0700

    script 8

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * print_array - prints an array
+ * @a: variable pointer
+ * @n: number of arrays
+ */
+
+void print_array(int *a, int n)
+{
+	for (a = 0; a < n; a++)
+	{
+		printf("%d, ", n[j]);
+	}
+	printf('\n');
+}
