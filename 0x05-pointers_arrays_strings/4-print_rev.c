938f76e0dab37b51d94d5e1a11a35e3be4e835eb
Afolabi Adetunji <adetunjiafolabi05@gmail.com>
2022-07-13 15:13:31 -0700

    script 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _print_rev - return the length of a string
+ * @s: variable
+ * Return: always 0
+ */
+void _print_rev(char *s)
+{
+	while (*s != '\0')
+	{
+		_putchar(*s);
+		s--;
+	}
+	_putchar('\n');
+}
