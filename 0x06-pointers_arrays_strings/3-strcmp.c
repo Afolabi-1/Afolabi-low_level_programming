c1246ca65f064dc368e6cb5b10708d9daa8db5a3
Afolabi Adetunji <adetunjiafolabi05@gmail.com>
2022-07-14 18:57:18 -0700

    fixed script 3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -11,15 +11,5 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	if (s1 < s2 || s2 > s1)
-	{
-		return (-15);
-	}
-	else if (s1 > s2 || s2 < s1)
-	{
-		return (15);
-	}
-	else if (s1 == s2)
-		return (0);
-	return (0);
+	return (strcmp(s1, s2));
 }
