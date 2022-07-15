e10a2238236e822b09a027ec97de40bdbf0fc209
Afolabi Adetunji <adetunjiafolabi05@gmail.com>
2022-07-14 18:48:09 -0700

    script 3'

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strcmp - compares two strings
+ * @s1: string 1
+ * @s2: string 2
+ *
+ * Return: always 0
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	if (s1 < s2)
+	{
+		return (-15);
+	}
+	else if (s1 > s2)
+	{
+		return (15);
+	}
+	else if (s1 == s2)
+		return (0);
+	return (0);
+}
