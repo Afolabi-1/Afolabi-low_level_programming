73fed30f3b16594fdad977d9002c308552ad95ca
Afolabi Adetunji <adetunjiafolabi05@gmail.com>
2022-07-18 19:58:14 -0700

    script 2

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include "string.h"
+#include "stdio.h"
+
+/**
+ * _strchr:  function that locates a character in a string
+ * @s: string s
+ * @c: character c
+ * Return: the first occurrence of c in s
+ */
+
+char *_strchr(char *s, char c)
+{
+	return (strchr(s, c));
+}
