f1f60d4a0dd06008ccc6d75ac611cc39fe0d5a19
Afolabi Adetunji <adetunjiafolabi05@gmail.com>
2022-07-20 20:31:28 -0700

    script 5

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+ * sqrt - loops through 1 - n to find the root
+ * @a: the perfet square
+ * @b: starting number that loops
+ * Return: -1 if no square root is found
+ * 1 if successful
+ */
+int sqrt(int a, int b)
+{
+	/**
+	 * doubles b and checks if it
+	 * is equal to a.
+	 */
+	if (b * b == a)
+		return (b);
+	/**
+	 * else if b < a then
+	 * a is not a perfect square
+	 */
+	else if (b * b > a)
+		return (-1);
+	/**
+	 * increments b
+	 */
+	return (sqrt(a, b + 1));
+}
+
+/**
+ * _sqrt_recursion - returns the natural square root of n
+ * @n: perfect square
+ *
+ * Return: 1 if successful,
+ * -1 if no squareroot is found.
+ */
+int _sqrt_recursion(int n)
+{
+	return (sqrt2(n, 1));
+}
