461a1863ec9a1ba15b826037d272c5c89c2c4a06
Afolabi Adetunji <adetunjiafolabi05@gmail.com>
2022-07-23 16:21:17 -0700

    script 4

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -15,12 +15,12 @@ int main(int argc, char **argv)
 	int a, b, sum = 0;
 	char *flag;
 
-	if(argc < 2)
+	if (argc < 2)
 	{
 		printf("0\n");
 		return (0);
 	}
-	
+
 	for (a = 1; argv[a]; a++)
 	{
 		b = strtol(argv[a], &flag, 10);
