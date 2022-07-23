dc73d4a89221e4aba707868522195304593f395b
Afolabi Adetunji <adetunjiafolabi05@gmail.com>
2022-07-22 21:17:00 -0700

    script 3

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints the multiplication of two numbers
+ * @argc: arg counts
+ * @argv: arg vector
+ *
+ * Return: Error and 1 if there is only 1 arguments,
+ * 0 if successful
+ */
+
+int main(int argc, char *argv[])
+{
+	int num1 = atoi(argv[1]);
+	int num2 = atoi(argv[2]);
+	int result = num1 * num2;
+
+	if (argc < 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	else
+		printf("%d\n", result);
+	return (0);
+}
