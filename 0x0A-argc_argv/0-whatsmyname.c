2cf5f21f8cd55798fbe0bc0eae98a3e8e6857023
Afolabi Adetunji <adetunjiafolabi05@gmail.com>
2022-07-22 20:46:32 -0700

    corrected betty error

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main (int argc, char *argv[])
+/**
+ * main - prints its name, followed by a new line.
+ * @argc: argument count
+ * @argv: arguments
+ *
+ * Return: 0
+ */
+int main(int argc, char *argv[])
 {
 	(void)argc;
 
