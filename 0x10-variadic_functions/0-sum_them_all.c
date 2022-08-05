7ff16a6bc13cb093116214b9e9989e2567277cb3
Afolabi Adetunji <adetunjiafolabi05@gmail.com>
2022-08-04 18:51:59 -0700

    fixed script 0

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -11,12 +11,12 @@ int sum_them_all(const unsigned int n, ...)
 {
 	va_list ap;
 	unsigned int i, j;
+
 	va_start(ap, n);
-	
-	if (n == 0)
+	if (in == 0)
 		return (0);
 	j = 0;
-	for ( i = 0; i < n; i++)
+	for (i = 0; i < n; i++)
 	{
 		j += va_arg(ap, unsigned int);
 	}
