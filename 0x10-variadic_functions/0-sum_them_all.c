4e8f60c2981d2fd80fe00b74114b85d229187eeb
Afolabi Adetunji <adetunjiafolabi05@gmail.com>
2022-08-04 18:45:31 -0700

    script 0

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdarg.h>
+
+/**
+ * sum_them_all - returns the sum of all its parameters
+ * @n: variable
+ * Return: the sum or 0 if n == 0
+ */
+
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list ap;
+	unsigned int i, j;
+	va_start(ap, n);
+	
+	if (n == 0)
+		return (0);
+	j = 0;
+	for ( i = 0; i < n; i++)
+	{
+		j += va_arg(ap, unsigned int);
+	}
+	va_end(ap);
+	return (j);
+}
--- README.md
+++
@@ -1,2 +0,0 @@
-# alx-low_level_programming
-I am starting c programming today.
