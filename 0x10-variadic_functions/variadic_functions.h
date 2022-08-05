6b49a595d53c96443c2ced9049f6f5d67e1f41ef
Afolabi Adetunji <adetunjiafolabi05@gmail.com>
2022-08-04 19:27:27 -0700

    updated variadic file

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,3 +1,22 @@
-int sum_them_all(const unsigned int n, ...);
-int _putchar(char c);
-void print_numbers(const char *separator, const unsigned int n, ...);
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdarg.h>
+/**
+ * struct print - print type with corresponding print function
+ * @t: print type
+ * @f: print function
+ */
+
+typedef struct print
+{
+	char *t;
+	void (*f)(va_list);
+} print_t;
+
+int sum_them_all(const unsigned int, ...);
+void print_numbers(const char *, const unsigned int, ...);
+void print_strings(const char *, const unsigned int, ...);
+void print_all(const char * const, ...);
+
+#endif
