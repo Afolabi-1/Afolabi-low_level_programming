40314c33a237eebf2aa6688d4255721cf5da9ae1
afolabi <72478870+Afolabi-1@users.noreply.github.com>
2022-09-30 01:29:40 +0100

    Update 4-free_dlistint.c

--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -1,15 +1,9 @@
-/*
- * File: 4-free_dlistint.c
- * Auth: Brennan D Baraban
- */
-
 #include "lists.h"
 
 /**
  * free_dlistint - Frees a linked dlistint_t list.
  * @head: The head of the dlistint_t list.
  */
- 
 void free_dlistint(dlistint_t *head)
 {
 	dlistint_t *tmp;
